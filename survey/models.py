1fb195c2831bb7e92623ec002e5c9f079f153122
Edwin Fuquen <efuquen@gmail.com>
2020-06-19 21:25:24 -0400

    Add base data to migration scripts.

--- survey/models.py
+++ survey/models.py
@@ -5,95 +5,56 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-def YesNoChoices():
-    STATUS = (
-        (0, "No"),
-        (1, "Yes"),
-        (99, "DK/REF")
-    )
-    return STATUS
+YES_NO_CHOICES = (
+    (0, "No"),
+    (1, "Yes"),
+    (99, "DK/REF")
+)
 
-class Ethnicity(models.Model):
-    STATUS = (
-        (1, "Hispanic"),
-        (2, "Non-Hispanic"),
-        (99, "Doesn't Know"),
-    )
-    ethnicity = models.IntegerField(choices=STATUS, default=1, null=True)
+ETHNICITY_CHOICES = (
+    (1, "Hispanic"),
+    (2, "Non-Hispanic"),
+    (99, "Doesn't Know"),
+)
 
-    def __str__(self):
-        return self.get_ethnicity_display()
+AGE_CHOICES = (
+    (0, "Under 18"),
+    (1, "18 - 24"),
+    (2, "25+"),
+    (99, "Not Sure"),
+)
 
+HOMELESS_CHOICES = (
+    (0, 'Definetly'),
+    (1, 'Possibly'),
+    (99, 'Not Sure'),
+)
 
-class Age(models.Model):
-    STATUS = (
-        (0, "Under 18"),
-        (1, "18 - 24"),
-        (2, "25+"),
-        (99, "Not Sure"),
-    )
-    age = models.IntegerField(choices=STATUS, default=1, null=True)
+GENDER_CHOICES = (
+    (0, "Male"),
+    (1, "Female"),
+    (99, "Not Sure"),
+)
 
-    def __str__(self):
-        return self.get_age_display()
-
-
-class Homeless(models.Model):
-    STATUS = (
-        (0, 'Definetly'),
-        (1, 'Possibly'),
-        (99, 'Not Sure'),)
-    is_homeless = models.IntegerField(default=1, choices=STATUS, null=True)
-
-    def __str__(self):
-        return self.get_is_homeless_display()
-
-
-class Gender(models.Model):
-    STATUS = (
-        (0, "Male"),
-        (1, "Female"),
-        (99, "Not Sure"),
-    )
-    gender = models.IntegerField(choices=STATUS, default=1, null=True)
-
-    def __str__(self):
-        return self.get_gender_display()
-
-
-class GenderDetailed(models.Model):
-    STATUS = (
+GENDER_DETAILED_CHOICES = (
         (0, "Male"),
         (1, "Female"),
         (2, "Transgender"),
         (3, "Gender Non-Conforming(i.e., not exclusively male or female)"),
         (99, "Not Sure"),
-    )
+)
 
-    genderdetailed = models.IntegerField(choices=STATUS, default=1, null=True)
+RACE_CHOICES = (
+    (0, "American Indian or Alaska Native"),
+    (1, "Asian"),
+    (2, "Black or African American"),
+    (3, "Native Hawaiian or Other Pacific Islander"),
+    (4, "White"),
+    (5, "Other"),
+    (99, "Not Sure")
+)
 
-    def __str__(self):
-        return self.get_genderdetailed_display()
-
-
-class Race(models.Model):
-    STATUS = (
-        (0, "American Indian or Alaska Native"),
-        (1, "Asian"),
-        (2, "Black or African American"),
-        (3, "Native Hawaiian or Other Pacific Islander"),
-        (4, "White"),
-        (5, "Other"),
-        (99, "Not Sure")
-    )
-    race = models.IntegerField(choices=STATUS, default=1, null=True)
-
-    def __str__(self):
-        return self.get_race_display()
-
-
-class LastNight(models.Model):
-    STATUS = (
+LAST_NIGHT_CHOICES = (
         (1, "Street or sidewalk"),
         (2, "Vehicle (car, van, RV, truck)"),
         (3, "Park"),
@@ -108,76 +69,122 @@ class LastNight(models.Model):
         (12, "House or apartment"),
         (13, "Jail, hospital, treatment program")
     )
-    lastnight = models.IntegerField(choices=STATUS, default=1, null=False)
+
+YES_NO_DK_CHOICES = (
+    (0, "No"),
+    (1, "Yes"),
+    (99, "DK/REF")
+)
+
+RELATIONSHIP_CHOICES = (
+    (0, "Self"),
+    (1, "Child"),
+    (2, "Spouse"),
+    (3, "Other Family"),
+    (4, "Non-Married Partner"),
+    (5, "Other, Non-Family")
+)
+
+HOMELESS_LENGTH_CHOICES = (
+    (0, "Days"),
+    (1, "Weeks"),
+    (2, "Months"),
+    (3, "Years"),
+    (99, "DK/REF")
+)
+
+TIMES_HOMELESS_CHOICE= (
+    (0, "Less than 4 times"),
+    (1, "4 or more times"),
+    (99, "DK/REF")
+)
+
+BARRIERS_CHOICES = (
+    (0, "(a) Alcohol use/Illegal drug use"),
+    (1, "(b) Psychiatric/emotional condition"),
+    (2, "(c) Physical disability"),
+    (3, "DK/REF")
+)
+
+class Ethnicity(models.Model):
+    ethnicity = models.IntegerField(choices=ETHNICITY_CHOICES, default=1, null=True)
+
+    def __str__(self):
+        return self.get_ethnicity_display()
+
+
+class Age(models.Model):
+    age = models.IntegerField(choices=AGE_CHOICES, default=1, null=True)
+
+    def __str__(self):
+        return self.get_age_display()
+
+
+class Homeless(models.Model):
+    is_homeless = models.IntegerField(default=1, choices=HOMELESS_CHOICES, null=True)
+
+    def __str__(self):
+        return self.get_is_homeless_display()
+
+
+class Gender(models.Model):
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=1, null=True)
+
+    def __str__(self):
+        return self.get_gender_display()
+
+
+class GenderDetailed(models.Model):
+    genderdetailed = models.IntegerField(choices=GENDER_DETAILED_CHOICES, default=1, null=True)
+
+    def __str__(self):
+        return self.get_genderdetailed_display()
+
+
+class Race(models.Model):
+    race = models.IntegerField(choices=RACE_CHOICES, default=1, null=True)
+
+    def __str__(self):
+        return self.get_race_display()
+
+
+class LastNight(models.Model):
+    lastnight = models.IntegerField(choices=LAST_NIGHT_CHOICES, default=1, null=False)
 
     def __str__(self):
         return self.get_lastnight_display()
 
 
 class YesNoDK(models.Model):
-    STATUS = (
-        (0, "No"),
-        (1, "Yes"),
-        (99, "DK/REF")
-    )
-
-    yesnodk = models.IntegerField(choices=STATUS, default=1, null=False)
+    yesnodk = models.IntegerField(choices=YES_NO_DK_CHOICES, default=1, null=False)
 
     def __str__(self):
         return self.get_yesnodk_display()
 
 
 class Relationship(models.Model):
-    STATUS = (
-        (0, "Self"),
-        (1, "Child"),
-        (2, "Spouse"),
-        (3, "Other Family"),
-        (4, "Non-Married Partner"),
-        (5, "Other, Non-Family")
-    )
-
-    relationship = models.IntegerField(choices=STATUS, default=0, null=False)
+    relationship = models.IntegerField(choices=RELATIONSHIP_CHOICES, default=0, null=False)
 
     def __str__(self):
         return self.get_relationship_display()
 
 
 class HomelessLength(models.Model):
-    STATUS = (
-        (0, "Days"),
-        (1, "Weeks"),
-        (2, "Months"),
-        (3, "Years"),
-        (99, "DK/REF")
-    )
-
-    homelesslength = models.IntegerField(choices=STATUS, default=2, null=False)
+    homelesslength = models.IntegerField(choices=HOMELESS_LENGTH_CHOICES, default=2, null=False)
 
     def __str__(self):
         return self.get_homelesslength_display()
 
 
 class TimesHomeless(models.Model):
-    STATUS = (
-        (0, "Less than 4 times"),
-        (1, "4 or more times"),
-        (99, "DK/REF")
-    )
 
-    timeshomeless = models.IntegerField(choices=STATUS, default=0, null=False)
+    timeshomeless = models.IntegerField(choices=TIMES_HOMELESS_CHOICE, default=0, null=False)
 
     def __str__(self):
         return self.get_timeshomeless_display()
 
 class Barriers(models.Model):
-    STATUS = (
-        (0, "(a) Alcohol use/Illegal drug use"),
-        (1, "(b) Psychiatric/emotional condition"),
-        (2, "(c) Physical disability"),
-        (3, "DK/REF")
-    )
-    barriers = models.IntegerField(choices=STATUS, default = 0, null=False)
+    barriers = models.IntegerField(choices=BARRIERS_CHOICES, default = 0, null=False)
 
     def __str__(self):
         return self.get_barriers_display()
@@ -239,16 +246,16 @@ class Observation(models.Model):
 
 #Only completed along Survey_Individual if the individual of the household is older than 18
 class Survey_IndividualExtra(models.Model):
-    client_survey_substance = models.IntegerField(choices=YesNoChoices(), help_text="Do you drink alcoholic beverages or use drugs\n(illegal or prescription for nonmedical reasons)?\n[IF NECESSARY: non-medical reasons means because of the experience or feeling the drug caused.]")
-    client_survey_mhealth = models.IntegerField(choices=YesNoChoices(), help_text="Do you have psychiatric or emotional conditions such as depression or schizophrenia?")
-    client_survey_phealth = models.IntegerField(choices=YesNoChoices(), help_text="Do you have a physical disability? This could include something that substantially limits one or more basic physical activities such as walking, climbing stairs, reaching, lifting, or carrying?")
+    client_survey_substance = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do you drink alcoholic beverages or use drugs\n(illegal or prescription for nonmedical reasons)?\n[IF NECESSARY: non-medical reasons means because of the experience or feeling the drug caused.]")
+    client_survey_mhealth = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do you have psychiatric or emotional conditions such as depression or schizophrenia?")
+    client_survey_phealth = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do you have a physical disability? This could include something that substantially limits one or more basic physical activities such as walking, climbing stairs, reaching, lifting, or carrying?")
     #If previous 3 questions were all NO or DK/REF, skip next 2 questions
-    client_survey_stablehousing = models.IntegerField(choices=YesNoChoices(), help_text="Do any of the situations we just discussed keep you from holding a job or living in stable housing?")
+    client_survey_stablehousing = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do any of the situations we just discussed keep you from holding a job or living in stable housing?")
     #if previous question was answered NO or DK/REF then skp next question
     client_survey_barriers = models.ManyToManyField(Barriers, help_text="Which ones keep you from holding a job or living in stable housing?")
-    client_survey_specialed = models.IntegerField(choices=YesNoChoices(), help_text="Have you ever received special education (or special ed.) services for an extended period of time?")
-    client_survey_HIVAIDS = models.IntegerField(choices=YesNoChoices(), help_text="Do you have AIDS or an HIV-related illness?")
-    client_survey_DV = models.IntegerField(choices=YesNoChoices(), help_text="Are you experiencing homelessness because you are currently fleeing domestic violence, dating violence, sexual assault, or stalking?")
+    client_survey_specialed = models.IntegerField(choices=YES_NO_CHOICES, help_text="Have you ever received special education (or special ed.) services for an extended period of time?")
+    client_survey_HIVAIDS = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do you have AIDS or an HIV-related illness?")
+    client_survey_DV = models.IntegerField(choices=YES_NO_CHOICES, help_text="Are you experiencing homelessness because you are currently fleeing domestic violence, dating violence, sexual assault, or stalking?")
 
     # for query display results
     def barriers_list(self):
@@ -259,20 +266,20 @@ class Survey_Individual(models.Model):
     client_survey_initials = models.CharField(max_length = 25, help_text = "What are your initials?", null=True)
     client_survey_relationship = models.ForeignKey(Relationship, on_delete=models.CASCADE, help_text= "How are you related to the Head of Household (select 'Self' for the HoH)", null=True)
     #skip next two for the head of household, only for other people in household
-    client_survey_hhconfirm = models.IntegerField(choices=YesNoChoices(), help_text="Are you staying with this person tonight?", null=True)
+    client_survey_hhconfirm = models.IntegerField(choices=YES_NO_CHOICES, help_text="Are you staying with this person tonight?", null=True)
     client_survey_nonhhlastnight = models.CharField(max_length = 100, help_text = "Where are you staying tonight?", null=True)
     client_survey_age_exact = models.IntegerField(help_text="How old are you? [ENTER NUMBER]", default=0, null=True)
     client_survey_age_grouped = models.ForeignKey(Age, on_delete=models.CASCADE, help_text= "[IF HESITANT ASK:] Are You?", null=True)
-    client_survey_ethnicity = models.IntegerField(choices=YesNoChoices(), help_text="Are you Hispanic or Latino?", null=True)
+    client_survey_ethnicity = models.IntegerField(choices=YES_NO_CHOICES, help_text="Are you Hispanic or Latino?", null=True)
     client_survey_race = models.ManyToManyField(Race, help_text="What is your race? You can select one or more races.\n[READ CATEGORIES, DO NOT READ 'Please Specify.']")
     client_survey_race_other = models.CharField(max_length = 50, help_text="Please Specify.", null=True)
     client_survey_gender = models.ForeignKey(GenderDetailed, on_delete=models.CASCADE, help_text="What is your gender?", null=True)
-    client_survey_served = models.IntegerField(choices=YesNoChoices(), help_text="Have you served in the United States Armed Force\n(Army, Navy, Air Force, Marine Corps, or Coast Guard)?", null=True)
+    client_survey_served = models.IntegerField(choices=YES_NO_CHOICES, help_text="Have you served in the United States Armed Force\n(Army, Navy, Air Force, Marine Corps, or Coast Guard)?", null=True)
     #if previous question is NO, ask the next 2 questions, if YES or DK/REF skip the next 3
-    client_survey_served_guard_res = models.IntegerField(choices=YesNoChoices(), help_text = "Were you ever called into active duty\nas a member of the National Guard or as a Reservist? ", null=True)
-    client_survey_served_VHA = models.IntegerField(choices=YesNoChoices(), help_text="Have you ever received health care or benefits from a Veterans Administration medical center?", null=True)
-    client_survey_benefits = models.IntegerField(choices=YesNoChoices(), help_text="Do you receive any disability benefits such as\nSocial Security Income, Social Security Disability Income,\nor Veteran’s Disability Benefits?", null=True)
-    client_surey_firsttime = models.IntegerField(choices=YesNoChoices(), help_text="Is this the first time you have been homeless?", null=True)
+    client_survey_served_guard_res = models.IntegerField(choices=YES_NO_CHOICES, help_text = "Were you ever called into active duty\nas a member of the National Guard or as a Reservist? ", null=True)
+    client_survey_served_VHA = models.IntegerField(choices=YES_NO_CHOICES, help_text="Have you ever received health care or benefits from a Veterans Administration medical center?", null=True)
+    client_survey_benefits = models.IntegerField(choices=YES_NO_CHOICES, help_text="Do you receive any disability benefits such as\nSocial Security Income, Social Security Disability Income,\nor Veteran’s Disability Benefits?", null=True)
+    client_surey_firsttime = models.IntegerField(choices=YES_NO_CHOICES, help_text="Is this the first time you have been homeless?", null=True)
     client_survey_homelesslength = models.ForeignKey(HomelessLength, on_delete=models.CASCADE, related_name="this_time", help_text="How long have you been homeless this time?\nOnly include time spent staying in shelters and/or on the streets.", null=True)
     client_survey_homelesslength_number = models.IntegerField(help_text="Enter number of days/weeks/months/Years:", null=True)
     #If client_survey_firsttime is YES skip the next two questions
