647a5dbbaf8e1265643ad00d1d020f69e0936a79
tim crowley <tcrowley@ufl.edu>
2020-06-24 18:42:45 -0400

    fixed login so it's functional. successful login redirects to User page

--- PIT_APP/views.py
+++ PIT_APP/views.py
@@ -18,7 +18,7 @@ def index(request):
     return render(request, 'base/home.html')
 
 
-def login(request, user):
+def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
@@ -26,9 +26,8 @@ def login(request, user):
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
-                login(request, user)
                 messages.info(request, f"You are now logged in as {username}")
-                return redirect('/')
+                return redirect('user1')
             else:
                 messages.error(request, "Invalid username or password.")
         else:
