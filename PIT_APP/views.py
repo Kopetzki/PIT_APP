bd949ae735b8fea14cf6766086dea4de6821c5ca
tim crowley <tcrowley@ufl.edu>
2020-06-24 14:33:41 -0400

    added login functionality

--- PIT_APP/views.py
+++ PIT_APP/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from django.urls import reverse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 
 # import the different classes
 
@@ -14,8 +18,23 @@ def index(request):
     return render(request, 'base/home.html')
 
 
-def login(request):
-    return render(request, 'base/login.html')
+def login(request, user):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, 'base/login.html', context={"form": form})
 
 
 def resources(request):
