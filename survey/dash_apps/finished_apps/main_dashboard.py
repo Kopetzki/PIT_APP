0c509201d12c483815bdeb4224c59a56db24e9b6
Edwin Fuquen <efuquen@gmail.com>
2020-06-30 01:30:47 -0400

    Call only one SQL query on chart page load.

--- survey/dash_apps/finished_apps/main_dashboard.py
+++ survey/dash_apps/finished_apps/main_dashboard.py
@@ -11,97 +11,98 @@ app = DjangoDash('main_dash', external_stylesheets=external_stylesheets)
 config = {'displayModeBar': False}
 
 
-def pop_count_graph():
+def pop_count_graph(inds):
     #graph that counts households + individuals
     figure = {
-        'data': [
-            {'x': ['Groups', 'Individuals'],
-             'y': [Observation.objects.all().count(), Observation_Individual.objects.all().count()], 'type': 'bar',
-             'name': 'Groups'},
-        ],
-        'layout': {
-            'title': 'Observed Households and Individuals',
-            'font': dict(
-                size = 12,
-                family = 'sans-serif'
-            )
-        }
+      'data': [
+        {'x': ['Groups', 'Individuals'],
+         'y': [inds.count(), inds.count()], 'type': 'bar',
+         'name': 'Groups'},
+      ],
+      'layout': {
+        'title': 'Observed Households and Individuals',
+        'font': dict(
+            size = 12,
+            family = 'sans-serif'
+        )
+      }
     }
 
     return figure
 
-def age_graph():
+def age_graph(inds):
     #simple pie chart for age groups
     age_lookup = {
-        1: "Under 18",
-        2: "18 - 24",
-        3: "25+",
-        4: "Not Sure"}
+      1: "Under 18",
+      2: "18 - 24",
+      3: "25+",
+      4: "Not Sure"
+    }
 
-    data_group = Observation_Individual.objects.all().values('client_age').annotate(dcount=Count('client_age'))
+    data_group = inds.values('client_age').annotate(dcount=Count('client_age'))
     data_values = []
     data_annotations = []
     for x in data_group:
-        data_values.append(list(x.values())[1])
-        y = list(x.values())[0]
-        data_annotations.append(age_lookup[y])
+      data_values.append(list(x.values())[1])
+      y = list(x.values())[0]
+      data_annotations.append(age_lookup[y])
     figure = {
-              "data": [
-                {
-                  "values": data_values,
-                  "labels": data_annotations,
-                  "textposition":"inside",
-                  "name": 'Age',
-                  "hoverinfo":"label+value+name",
-                  "hole": .5,
-                  "type": "pie"
-                }],
-              "layout": {
-                    "title":"Age Groups",
-                    "font" : dict(
-                                size = 12,
-                                family = 'sans-serif'
-                            ),
-                }
-            }
+      "data": [
+        {
+          "values": data_values,
+          "labels": data_annotations,
+          "textposition":"inside",
+          "name": 'Age',
+          "hoverinfo":"label+value+name",
+          "hole": .5,
+          "type": "pie"
+        }],
+        "layout": {
+          "title":"Age Groups",
+          "font" : dict(
+            size = 12,
+            family = 'sans-serif'
+          ),
+        }
+      }
     return figure
 
-def gender_graph():
+def gender_graph(inds):
     #simple gender breakdown
     gender_lookup = {
-        1: "Male",
-        2: "Female",
-        3: "Not Sure",
+      1: "Male",
+      2: "Female",
+      3: "Not Sure",
     }
-    data_group = Observation_Individual.objects.all().values('client_gender').annotate(dcount=Count('client_gender'))
+    data_group = inds.values('client_gender').annotate(dcount=Count('client_gender'))
     data_values = []
     data_annotations = []
     for x in data_group:
-        data_values.append(list(x.values())[1])
-        y = list(x.values())[0]
-        data_annotations.append(gender_lookup[y])
+      data_values.append(list(x.values())[1])
+      y = list(x.values())[0]
+      data_annotations.append(gender_lookup[y])
     figure = {
-              "data": [
-                {
-                  "values": data_values,
-                  "labels": data_annotations,
-                  "textposition":"inside",
-                  "name": 'Gender',
-                  "hoverinfo":"label+value+name",
-                  "hole": .5,
-                  "type": "pie"
-                }],
-              "layout": {
-                    "title":"Gender Groups",
-                    "font" : dict(
-                                size = 12,
-                                family = 'sans-serif'
-                            ),
-                }
-            }
+      "data": [
+        {
+          "values": data_values,
+          "labels": data_annotations,
+          "textposition":"inside",
+          "name": 'Gender',
+          "hoverinfo":"label+value+name",
+          "hole": .5,
+          "type": "pie"
+        }],
+      "layout": {
+        "title":"Gender Groups",
+        "font" : dict(
+          size = 12,
+          family = 'sans-serif'
+        ),
+      }
+    }
     return figure
 
-def race_graph():
+def race_graph(inds):
     #pie chart for race
     race_lookup = {
         1: "American Indian or Alaska Native",
@@ -113,7 +114,7 @@ def race_graph():
         7: "Not Sure"
          }
 
-    data_group = Observation_Individual.objects.all().values('client_race').annotate(dcount=Count('client_race'))
+    data_group = inds.values('client_race').annotate(dcount=Count('client_race'))
     data_values = []
     data_annotations = []
     for x in data_group:
@@ -121,41 +122,41 @@ def race_graph():
         y = list(x.values())[0]
         data_annotations.append(race_lookup[y])
     figure = {
-              "data": [
-                {
-                  "values": data_values,
-                  "labels": data_annotations,
-                  "textposition":"inside",
-                  "name": 'Race',
-                  "hoverinfo":"label+value+name",
-                  "hole": .5,
-                  "type": "pie"
-                }],
-              "layout": {
-                    "title":"Race",
-                    "font" : dict(
-                                size = 12,
-                                family = 'sans-serif'
-                            ),
-                }
-            }
+      "data": [
+        {
+          "values": data_values,
+          "labels": data_annotations,
+          "textposition":"inside",
+          "name": 'Race',
+          "hoverinfo":"label+value+name",
+          "hole": .5,
+          "type": "pie"
+        }],
+      "layout": {
+            "title":"Race",
+            "font" : dict(
+                        size = 12,
+                        family = 'sans-serif'
+                    ),
+        }
+    }
     return figure
 
 def layout():
-    individuals = Observation_Individual.objects.all()
-    if individuals.count() > 0:
+    inds = Observation_Individual.objects.all()
+    if inds.count() > 0:
       return html.Div(children=[
           html.Div(children=[
               html.Div([dcc.Graph(
                   id='groups-individuals',
-                  figure=pop_count_graph(),
+                  figure=pop_count_graph(inds),
                   config=config
               )],
                   style={'display': 'inline-block', 'width': '49%'}),
 
               html.Div([dcc.Graph(
                   id='age-groups',
-                  figure=age_graph(),
+                  figure=age_graph(inds),
                   config=config)
               ], style={'display': 'inline-block', 'width': '49%'})
           ], className='row'),
@@ -163,14 +164,14 @@ def layout():
           html.Div(children=[
               html.Div([dcc.Graph(
                   id='gender-groups',
-                  figure=gender_graph(),
+                  figure=gender_graph(inds),
                   config=config
               )],
                   style={'display': 'inline-block', 'width': '49%'}),
 
               html.Div([dcc.Graph(
                   id='race-groups',
-                  figure=race_graph(),
+                  figure=race_graph(inds),
                   config=config)
               ], style={'display': 'inline-block', 'width': '49%'})
           ], className='row')
