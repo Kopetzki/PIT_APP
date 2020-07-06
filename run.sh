3029a6b1170a9678725ba9ed601f1c9ca4740f25
efuquen <efuquen@gmail.com>
2020-07-06 17:03:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 #
 # By default the db.sqlite3 database file is created in the
 # site-packages folder of your Python distribution. To have it be
-# created in a different folder use the DATABASE_HOME environment
+# created in a different folder use the DATABASE_NAME environment
 # variable. For example the following commands:
 #
-# DATABASE_HOME="./db.sqlite3" ./run.sh
+# DATABASE_NAME="./db.sqlite3" ./run.sh
 #
 # will create the the sqlite file in whatever directory you call run.sh
-# from.
+# from, or use a prexisting file if already exists there.
 
 pip3 install django-pit-survey -U
 pit_init
