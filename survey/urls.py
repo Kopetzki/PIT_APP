6c769bb4df7eb2f9eb01281825a44650279a4a1f
Edwin Fuquen <efuquen@gmail.com>
2020-08-01 20:26:34 -0400

    Comment out debug toolbar for release.

--- survey/urls.py
+++ survey/urls.py
@@ -53,8 +53,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+#if settings.DEBUG:
+#    import debug_toolbar
+#    urlpatterns = [
+#        path('__debug__/', include(debug_toolbar.urls)),
+#    ] + urlpatterns
